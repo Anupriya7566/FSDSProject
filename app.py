28298c5076a6b261064651bf028d6b89eef0109e
Anu Priya <anupriyasharma326@gmail.com>
2024-02-10 20:19:11 +0530

    add html file

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
-import os
+from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData , PredictPipeline
+
+from flask import Flask,request,render_template,jsonify
+
+
+app=Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+
+@app.route("/predict",methods=["GET", "POST"])
+def predict_datapoints():
+    if request.method == "GET":
+        return render_template("form.html")
+    
+    
+    
+    
+    else:
+        pass
+
+
+
+
+
+
+
+    
+app.run()
+
